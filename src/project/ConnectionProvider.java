dcf7fd6a19ac17033ba1f0468c3f503051a5c076
chaitanyashimpi <chaitanyashimpi2001@gmail.com>
2022-04-12 10:52:06 +0530

    Completed Project

---
+++ src/project/ConnectionProvider.java
@@ -0,0 +1,18 @@
+
+package project;
+
+import java.sql.*;
+
+
+public class ConnectionProvider {
+       public static Connection getCon(){
+           try{
+               Class.forName("com.mysql.cj.jdbc.Driver");
+               Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gms", "root", "");
+               return con;
+           }
+           catch(ClassNotFoundException | SQLException e){
+               return null;
+           }
+       }
+}
